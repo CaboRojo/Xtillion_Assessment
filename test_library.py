72c8852f2840d768b967961ff16f1e9feddcea4b
Alexis Sanchez - El Pecoso <alexis.sanchez@student.ie.edu>
2024-12-10 22:59:01 +0100

    submitting all of my files

---
+++ test_library.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTE
+import xgboost as xgb
+# import lightgbm as lgb
+import shap
+import neptune
+
+print("All libraries loaded successfully!")
